10329a3328cea60f68b2178eb847eaec64f409f6
Cristhian Daza <cristian.d.b@hotmail.com>
2025-11-27 23:18:24 -0500

    fix(style): adjust minimum width of tv-setting panel from 14rem to 5rem

--- src/demo/Demo.vue
+++ src/demo/Demo.vue
@@ -14,7 +14,7 @@ const TvSettings = defineAsyncComponent(/* webpackChunkName: "tvSettings" */() =
     npm-install="@todovue/tv-settings"
     source-link="https://github.com/TODOvue/tv-settings"
     url-clone="https://github.com/TODOvue/tv-settings.git"
-    version="1.0.0"
+    version="1.0.1"
   />
 </template>
 
